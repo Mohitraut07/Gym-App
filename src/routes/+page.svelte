9f21255339b1d5f0544f971e513c55f25da689ba
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-09-14 22:14:51 +0530

    feat: Add ReviewCard and Reviews components
    - Add ReviewCard component for displaying individual review cards
    - Add Reviews component for displaying multiple review cards
    - Update page layout to include Reviews component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
   import Hero from "../components/Hero.svelte";
   import "../app.css";
   import Product from "../components/Product.svelte";
+  import Reviews from "../components/Reviews.svelte";
 </script>
 <main>
   <Hero/>
   <Product/>
+  <Reviews/>
 </main>

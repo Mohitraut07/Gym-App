731416675ff0c2b1982151cee7626e02e0e048b3
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-09-14 19:07:51 +0530

    feat: Add drop shadow to special button and product card
    - Added drop shadow to the special button in app.css
    - Added dropShadow class to the ProductCard component in ProductCard.svelte
    - Updated the Product component to import and use the ProductCard component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import Hero from "../components/Hero.svelte";
   import "../app.css";
+  import Product from "../components/Product.svelte";
 </script>
 <main>
   <Hero/>
+  <Product/>
 </main>

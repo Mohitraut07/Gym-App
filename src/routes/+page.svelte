83fcd91eb4930b613ccf8c699a48fa9488960832
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-09-19 12:53:53 +0530

    feat: Add FAQs component for frequently asked questions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
   import "../app.css";
   import Product from "../components/Product.svelte";
   import Reviews from "../components/Reviews.svelte";
+  import FaQs from "../components/FAQs.svelte";
 </script>
 <main>
   <Hero/>
   <Product/>
   <Reviews/>
+  <FaQs/>
 </main>
